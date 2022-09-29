012e0e0b9eda9a43104e87458935bd70ef3932e3
Brooke Finister <brookeafinister@gmail.com>
2022-09-29 00:34:26 -0700

    cart semi functional
    Need to fix remove button functionality

--- scripts.js
+++ scripts.js
@@ -40,19 +40,36 @@ const itemList = document.getElementById("item-list")
 const cartQty = document.getElementById("cart-qty")
 const cartTotal = document.getElementById("cart-total")
 const cart = []
+// -----------------------------------------------------------------------------------
+// HAndle Change Events on Update input
+
+itemList.onchange = function(e) {
+  if (e.target && e.target.classList.contains("Update")) {
+    const name = e.target.dataset.name
+    const qty = parseInt(e.target.value)
+    updateCart(name, qty)
 
 
+  }
+}
 // -----------------------------------------------------------------------------------
 //  Handle Clicks on Lists
+
 itemList.onclick = function(e) {
-    if (e.target && e.target.classList.contains("remove")) {
-       const name = e.target.dataset.name
-        removeItem(name)
-
-    }
-    }
-
+  if (e.target && e.target.classList.contains("remove")) {
+    const name = e.target.dataset.name
+    removeItem(name)
+  } else if (e.target && e.target.classList.contains("add-one")) {
+    const name = e.target.dataset.name
+    addItem(name)
+  } else if (e.target && e.target.classList.contains("remove-one")) {
+    const name = e.target.dataset.name
+    removeItem(name, 1)
+  }
 }
+
+
+
 // -----------------------------------------------------------------------------------
 // Function to Add Item
 
@@ -64,7 +81,11 @@ function addItem(name, price) {
     }
   }
 
-  const item = {name, price, qty: 1}
+  const item = {
+    name,
+    price,
+    qty: 1
+  }
   cart.push(item)
 }
 
@@ -73,29 +94,36 @@ function addItem(name, price) {
 
 function showItems() {
   const qty = getQty()
-//   console.log(`You have ${qty} items in your cart`)
+  //   console.log(`You have ${qty} items in your cart`)
   cartQty.innerHTML = `You have ${qty} items in your cart`
 
   let itemStr = " "
   for (let i = 0; i < cart.length; i += 1) {
 
     // console.log(`${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
-    
-    const { name, price, qty } = cart[i]
+
+    const {
+      name,
+      price,
+      qty
+    } = cart[i]
 
     itemStr += `<li>
     ${name} $${price} x ${qty} = ${qty * price}
-    <button class="remove" data-name"${name}">Remove</button>
+    <button class="remove" data-name"${name}">Remove </button>
+    <button class="remove" data-name"${name}"> + </button>
+    <button class="add-one" data-name"${name}"> - </button>
+    <input class ="update" type="number" data-name ="${name}">
     </li>`
-      
-    
+
+
   }
 
   itemList.innerHTML = itemStr
 
-//   console.log(`Total in cart: $${getTotal()}`)
+  //   console.log(`Total in cart: $${getTotal()}`)
   cartTotal.innerHTML = `Total in cart: $${getTotal()}`
-  
+
 }
 
 
@@ -128,12 +156,23 @@ function getTotal() {
 function removeItem(name, qty = 0) {
   for (let i = 0; i < cart.length; i += 1) {
     if (cart[i].name === name) {
-        if (qty > 0) {
+      if (qty > 0) {
         cart[i].qty -= qty
-        }
-        if (cart[i].qty < 1 || qty === 0) {
+      }
+      if (cart[i].qty < 1 || qty === 0) {
         cart.splice(i, 1)
       }
+      showItems()
+      return
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------------
+function updateCart(name, qty) {
+  for (let i = 0; i < cart.length; i += 1) {
+    if (cart[i].name === name) {
+      cart[i].qty = qty
       showItems()
       return
     }
@@ -144,25 +183,25 @@ function removeItem(name, qty = 0) {
 
 const all_items_button = Array.from(document.querySelectorAll("button"));
 all_items_button.forEach(elt => elt.addEventListener('click', () => {
-    addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
-    showItems()
-  }))
+  addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
+  showItems()
+}))
 // -----------------------------------------------------------------------------------
 // Test Code
 
-addItem("Happiness", 5.99)
-addItem("Sadness", 5.99)
-addItem("Anger", 5.99)
-addItem("Calmness", 5.99)
-addItem("Curiousness", 5.99)
-addItem("Disgust", 5.99)
-addItem("Energy", 5.99)
-addItem("Depression", 5.99)
-addItem("Fearfulness", 5.99)
-addItem("Shyness", 5.99)
-addItem("Sleep", 5.99)
-addItem("Surprise", 5.99)
-addItem("Surprise", 5.99)
+// addItem("Happiness", 5.99)
+// addItem("Sadness", 5.99)
+// addItem("Anger", 5.99)
+// addItem("Calmness", 5.99)
+// addItem("Curiousness", 5.99)
+// addItem("Disgust", 5.99)
+// addItem("Energy", 5.99)
+// addItem("Depression", 5.99)
+// addItem("Fearfulness", 5.99)
+// addItem("Shyness", 5.99)
+// addItem("Sleep", 5.99)
+// addItem("Surprise", 5.99)
+// addItem("Surprise", 5.99)
 
-showItems()
-console.log(itemList)
+// showItems()
+// console.log(itemList)
